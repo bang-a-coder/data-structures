e2e5a849b3ae623780e2538db6b1b86896ae23a9
bang-a-coder <nikitas4717@gmail.com>
2021-10-14 23:06:31 +0200

    heapSort works bitch

--- nheaps.py
+++ nheaps.py
@@ -21,12 +21,14 @@ sample2 = [7,6,10]
 sample3 = [10,6,7,5,15,17,12,6,3,16] #incomplete tree
 
 
-def maxHeapify(arr, i): #maintain max heap property
-	print(arr, i)
+def maxHeapify(arr, i, heapSize = None): #maintain max heap property
+	# print(arr, i)
+	print(heapSize)
 	le = getLeftKey(i)	
 	ri = getRightKey(i)
-
-	heapSize = len(arr) - 1 
+	
+	if heapSize == None:
+		heapSize = len(arr) - 1
 	
 	if le <= heapSize and arr[le] > arr[i]:
 		larger = le
@@ -36,12 +38,12 @@ def maxHeapify(arr, i): #maintain max heap property
 	if ri <= heapSize and arr[ri] > arr[larger]:
 		larger = ri
 	
-	print(larger, i)
+	# print(larger, i)
 	if larger != i:
 		smaller = arr[i]
 		arr[i] = arr[larger]
 		arr[larger] = smaller
-		maxHeapify(arr, larger)
+		maxHeapify(arr, larger, heapSize)
 
 
 # maxHeapify(sample3,0)
@@ -51,10 +53,23 @@ def buildMaxHeap(arr):
 	for i in range(math.floor(len(arr)/2), -1, -1):
 		maxHeapify(arr,i)
 
-	
-buildMaxHeap(sample3)
-print(sample3, isMaxHeap(sample3))
+def heapSort(arr):
+	buildMaxHeap(arr)
+	heapSize = len(arr)-1
+	for i in range(len(arr)-1,-1,-1):
+		# print(len(arr), i)	
+		root = arr[0]
+		arr[0] = arr[i]
+		arr[i] = root
+		heapSize = heapSize -1
+		maxHeapify(arr,0, heapSize)
+
 		
+# buildMaxHeap(sample3)
+# print(sample3, isMaxHeap(sample3))
+		
+heapSort(sample3)
+print(sample3)
 		
 
 
